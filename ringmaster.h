bb9a0b1ba2d0bf637970bb9f95b29441cdbb414c
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-02-22 13:53:13 -0500

    add skeleton

--- ringmaster.h
+++ ringmaster.h
@@ -1,4 +1,8 @@
 #ifndef __RINGMASTER_H__
 #define __RINGMASTER_H__
 
+class Ringmaster {
+
+};
+
 #endif
--- socket.h
+++ socket.h
@@ -1,6 +1,8 @@
 #ifndef __SOCKET_H__
 #define __SOCKET_H__
 
+class Socket {
 
+};
 
 #endif
